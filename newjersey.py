254024932b2b7cbab46300488341d19000eac5c4
Casey Rand <casrand@gmail.com>
2023-05-25 10:59:46 -0500

    Faster parsing of PDF titles. Adjusts upper bound dynamically

--- newjersey.py
+++ newjersey.py
@@ -23,18 +23,18 @@ class IGaming(NewJersey):
         num_pages = self.get_pages()
         # Gather data from each page.
         out = []
-        for i in range(num_pages):
-            casino = self.get_casino(i)
-            table = (self.get_first_table(i).
-                     replace(r'[$ \n]', '', regex=True))
-            row = table.iloc[1:,-1].str.rstrip('-')
+        for i, casino in zip(range(num_pages), self.get_casinos()):
+            #table = (self.get_first_table(i).
+            #         replace(r'[$ \n]', '', regex=True))
+            #row = table.iloc[1:,-1].str.rstrip('-')
             out.append({'State': self.state,
                         'Category': self.category,
                         'Date': self.timestamp,
                         'Provider': casino,
-                        'Online Poker': row[1],
-                        'Online Casino': row[2],
-                        'Total': row[3]})
+                        'Bound': bound})
+                        #'Online Poker': row[1],
+                        #'Online Casino': row[2],
+                        #'Total': row[3]})
         return pd.DataFrame(out)
 
     def read_pdf(self):
@@ -49,13 +49,21 @@ class IGaming(NewJersey):
         """ Gets the number of pages from temp storage. """
         return len(PdfReader(self.temp_storage).pages)
 
-    def get_casino(self, page_num, bound=705):  # 835 used to work and 730
+    def get_casinos(self):
         """ Open pdfium on page, getting casino header text only. """
         pdf = pdfium.PdfDocument(self.temp_storage)
-        page = pdf[page_num]
-        textpage = page.get_textpage()
-        text = textpage.get_text_bounded(bottom=bound).removeprefix('INTERNET WIN - CURRENT MONTH')
-        return text.split('MONTHLY')[0].replace('\r\n', '')
+        casinos = []
+        for page in pdf:
+            textpage = page.get_textpage()
+            casino = ''
+            # Using a flexible bound has proven the most successful. 2022 formatting in particular is unreliable.
+            bound = 900
+            while casino == '':
+                bound -= 50
+                text = textpage.get_text_bounded(bottom=bound).removeprefix('INTERNET WIN - CURRENT MONTH')
+                casino = text.split('MONTHLY')[0].replace('\r\n', '')
+            casinos.append((casino, bound))
+        return casinos
 
     def get_first_table(self, page_num):
         """ Open camelot on page, getting first table. """
@@ -65,15 +73,15 @@ class IGaming(NewJersey):
 
 if __name__ == '__main__':
     url = 'https://www.njoag.gov/about/divisions-and-offices/division-of-gaming-enforcement-home/financial-and-statistical-information/monthly-internet-gross-revenue-reports/'
-    igaming = bobs.get_links(url, href_keys=['IGRTaxReturns'])
+    igaming = bobs.get_links(url, href_keys=['IGRTaxReturns'])[:28]  # TODO no parsing past 2019.
     out_df = []
     for link in igaming:
         try:
             out_df.append(IGaming(link).clean())
-            print(f'Success-{link}')
+            print(f'Success - {link}')
         except bobs.InvalidExtraction as e:
             print(e.message)
         except:
-            print(f'Broken-{link}')
+            print(f'Broken - {link}')
     df = pd.concat(out_df)
     df.to_excel('New Jersey (iGaming).xlsx', index=False)
