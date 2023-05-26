7f58c750f77af7bbb6104ff899cac02a73cf166b
Casey Rand <casrand@gmail.com>
2023-05-26 17:10:59 -0500

    West Virginia completed

--- westvirginia.py
+++ westvirginia.py
@@ -1,118 +1,110 @@
 import bobs
+import requests
 import pandas as pd
-from datetime import datetime
+from io import BytesIO
+from zipfile import ZipFile
 
 class WestVirgina(bobs.Table):
     state = 'West Virginia'
 
-    def __init__(self, link):
-        self.link = link
-        self.df = pd.read_excel(link, skiprows=3)
-
-    def get_providers(self, key):
-        """ Get values above keys as providers. """
-        indexes = self.df[self.df.iloc[:,0] == key].index - 1
-        return self.df.iloc[indexes,0].to_list()
-
-    def clean(self):
-        """ Clean an Excel sheet. clean_row should change for type of table. """
-        out_df = []
-        for month, row in self.body.iterrows():
-            month = datetime.strptime(month, '%B %Y')
-            i_row = iter(row)
-            idx = 0
-            while idx < len(self.providers):
-                out_df.append(self.clean_row(idx, i_row, month))
-                idx += 1
-        return pd.DataFrame(out_df)
+    def __init__(self, zipfile):
+        self.zip = zipfile
+        self.filenames = [file.filename for file in self.zip.filelist]
 
 
 class IGaming(WestVirgina):
     category = 'iGaming'
+
+    def __init__(self, zipfile):
+        super().__init__(zipfile)
+        self.sheetnames = ['Mountaineer', 'Charles Town', 'Greenbrier']
     
-    def __init__(self, link):
-        super().__init__(link)
-        self.parse_columns = ['Wagers Received', 'Amount Won', 'Gross Revenue', 'Revenue (Rake & Tournament Fees)']
-        self.providers = self.get_providers('Interactive Slots')
-        self.body = self.df.loc[self.df.isin(self.parse_columns).any(axis=1)]
-        self.body = self.body.dropna(how='all', axis=1).T.iloc[1:-1]
-
     def clean(self):
-        return super().clean().explode(['Sub-Category', 'Wagers Received', 'Amount Won', 'Gross Revenue'])
-
-    def clean_row(self, idx, i_row, month):
-        # 1 - wagers, 2 - amount won, 3 - revenue
-        islots_1, islots_2, islots_3 = next(i_row), next(i_row), next(i_row)
-        ibanking_1, ibanking_3 = next(i_row), next(i_row)
-        nbanking_3 = next(i_row)
-        return {'State': self.state,
-                'Category': self.category,
-                'Sub-Category': ['Interactive Slots', 'Banking Tables', 'Non-Banking Tables (Poker)'],
-                'Date': month,
-                'Provider': self.providers[idx],
-                'Wagers Received': [islots_1, ibanking_1, pd.NA],
-                'Amount Won': [islots_2, pd.NA, pd.NA],
-                'Gross Revenue': [islots_3, ibanking_3, nbanking_3]}
+        dataframes = []
+        for file in self.filenames:
+            for sheet in self.sheetnames:
+                df = pd.read_excel(self.zip.open(file), sheet_name=sheet, skiprows=2)
+                # Clean columns.
+                df.columns = df.columns.str.rstrip('* ')
+                df = df.rename(columns={'Week Ending': 'Date', 'Paids': 'Amount Won'})
+                # Get relevant dates.
+                df = df.replace(r'[\* ]', '', regex=True)
+                df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y', errors='coerce').dropna()
+                grouped_df = df.groupby(pd.Grouper(key='Date', freq='MS')).sum()
+                grouped_df.reset_index(inplace=True)
+                grouped_df.insert(0, 'State', self.state)
+                grouped_df.insert(1, 'Category', self.category)
+                grouped_df.insert(3, 'Provider', sheet)
+                dataframes.append(grouped_df)
+        return pd.concat(dataframes)[['State', 'Category', 'Date', 'Provider', 'Wagers', 'Amount Won', 'Revenue']]
 
     @staticmethod
-    def save(df): 
-        df.index.name = 'Index'
+    def save(df):
         df = df.replace(0, pd.NA)
-        df = df.dropna(how='all', subset=['Wagers Received', 'Amount Won', 'Gross Revenue'])
-
-        df = df.sort_values(by=['Date', 'Index'], ascending=True)
+        df = df.dropna(subset=['Wagers', 'Amount Won', 'Revenue'], how='all')
+        df = df.sort_values(by=['Date', 'Provider'], ascending=True)
         df.to_excel('West Virginia (iGaming).xlsx', index=False)
 
 
 class Sports(WestVirgina):
     category = 'Online Sports Betting (OSB)'
-
-    def __init__(self, link):
-        super().__init__(link)
-        self.df.iloc[:,0] = self.df.iloc[:,0].str.rstrip('*')
-        self.parse_columns = ['Handle', 'Revenue', 'Promotional Credits', 'Gross Revenue (Taxable)']
-        self.providers = self.get_providers('Total Sports Wagering')
-        self.body = self.df.loc[self.df.isin(self.parse_columns).any(axis=1)]
-        self.body = self.body.dropna(how='all', axis=1).T.iloc[1:-3]
+    
+    def __init__(self, zipfile):
+        super().__init__(zipfile)
+        self.sheetnames = ['Mountaineer', 'Wheeling', 'Mardi Gras', 'Charles Town', 'Greenbrier']
 
     def clean(self):
-        return super().clean().explode(['Sub-Category', 'Handle', 'Revenue', 'Promotional Credits', 'Gross Revenue'])
+        dataframes = []
+        for file in self.filenames:
+            for sheet in self.sheetnames:
+                df = pd.read_excel(self.zip.open(file), sheet_name=sheet, skiprows=3)
+                df = df.rename(columns={df.columns[0]: 'Date'})
+                # Get relevant dates.
+                df = df.replace(r'[\* ]', '', regex=True)
+                df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y', errors='coerce').dropna()
+                df = df.dropna(how='all', axis=1).dropna()
+                
+                # Parse sub-categories.
+                retail = df.iloc[:,:5]
+                retail.columns = ['Date', 'Gross Tickets Written', 'Voids', 'Tickets Cashed', 'Total Taxable Receipts']
+                retail.insert(0, 'Sub-Category', 'Retail')
+                online = df.iloc[:,[0,5,6,7,8]]
+                online.columns = ['Date', 'Gross Tickets Written', 'Voids', 'Tickets Cashed', 'Total Taxable Receipts']
+                online.insert(0, 'Sub-Category', 'Online')
+                total = df.iloc[:,[0,9,10,11,12]]
+                total.columns = ['Date', 'Gross Tickets Written', 'Voids', 'Tickets Cashed', 'Total Taxable Receipts']
+                total.insert(0, 'Sub-Category', 'Total')
+                combined_df = pd.concat([retail, online, total], ignore_index=True)
 
-    def clean_row(self, idx, i_row, month):
-        # 1 - handle, 2 - revenue, 3 - promotional, 4 - gross revenue
-        total_1, total_2, total_3, total_4 = next(i_row), next(i_row), next(i_row), next(i_row)
-        retail_1, retail_4 = next(i_row), next(i_row)
-        online_1, online_2, online_3, online_4 = next(i_row), next(i_row), next(i_row), next(i_row)
-        return {'State': self.state,
-                'Category': self.category,
-                'Sub-Category': ['Total', 'Retail', 'Online'],
-                'Date': month,
-                'Provider': self.providers[idx],
-                'Handle': [total_1, retail_1, online_1],
-                'Revenue': [total_2, retail_4, online_2],
-                'Promotional Credits': [total_3, pd.NA, online_3],
-                'Gross Revenue': [total_4, retail_4, online_4]}
+                # Set Date as index for Grouper.
+                combined_df = combined_df.set_index('Date')
+                grouped_df = combined_df.groupby([pd.Grouper(freq='MS'), 'Sub-Category']).sum()
+                grouped_df.reset_index(inplace=True)
+                grouped_df.insert(0, 'State', self.state)
+                grouped_df.insert(1, 'Category', self.category)
+                grouped_df.insert(4, 'Provider', sheet)
+                dataframes.append(grouped_df)
+        return pd.concat(dataframes)[['State', 'Category', 'Sub-Category', 'Date', 'Provider', 'Gross Tickets Written', 'Voids', 'Tickets Cashed', 'Total Taxable Receipts']]
 
     @staticmethod
     def save(df):
-        df.index.name = 'Index'
         df = df.replace(0, pd.NA)
-        df = df.dropna(how='all', subset=['Handle', 'Revenue', 'Promotional Credits', 'Gross Revenue'])
-        
+        df = df.dropna(how='all', subset=['Gross Tickets Written', 'Voids', 'Tickets Cashed', 'Total Taxable Receipts'])
         df['Sub-Category'] = df['Sub-Category'].astype('category').cat.set_categories(['Retail', 'Online', 'Total'])
-        df = df.sort_values(by=['Date', 'Index', 'Sub-Category'], ascending=True)
+        df = df.sort_values(by=['Date', 'Provider', 'Sub-Category'], ascending=True)
         df.to_excel('West Virginia (OSB).xlsx', index=False)
 
 
 if __name__ == '__main__':
-    sports = []
-    igaming = []
-    for i in range(2019, 2023):
-        sports.append(f'https://gamingcontrolboard.pa.gov/files/revenue/Gaming_Revenue_Monthly_Sports_Wagering_FY{i}{i+1}.xlsx')
-        igaming.append(f'https://gamingcontrolboard.pa.gov/files/revenue/Gaming_Revenue_Monthly_Interactive_Gaming_FY{i}{i+1}.xlsx')
+    sports_zip = bobs.get_links('https://wvlottery.com/requests/2020-06-15-1110/?report=new', text_keys='Sports Wagering')[0]
+    igaming_zip = bobs.get_links('https://wvlottery.com/requests/2020-06-15-1110/?report=new', text_keys='iGaming')[0]
 
-    igaming_cleaned = [IGaming(i).clean() for i in igaming]
-    IGaming.save(pd.concat(igaming_cleaned))
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36'}
+    szip = ZipFile(BytesIO(requests.get(sports_zip, headers=HEADERS).content))
+    izip = ZipFile(BytesIO(requests.get(igaming_zip, headers=HEADERS).content))
 
-    sports_cleaned = [Sports(s).clean() for s in sports]
-    Sports.save(pd.concat(sports_cleaned))
+    df = Sports(szip).clean()
+    Sports.save(df)
+
+    df = IGaming(izip).clean()
+    IGaming.save(df)
