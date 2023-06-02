9b09f314953949e38b5a8c3f8779878592c60e35
Casey Rand <casrand@gmail.com>
2023-06-02 13:26:52 -0500

    Working for Indiana and numerics

--- scraper.py
+++ scraper.py
@@ -31,8 +31,44 @@ class Table:
     def to_numeric(df, cols):
         """ Tries to make certain columns in a dataframe numeric. Removes commas. """
         # Remove commas.
-        df[cols] = df[cols].replace(r',', '', regex=True)
+        df[cols] = df[cols].replace(r'[$,()]', '', regex=True)
         df[cols] = df[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+
+    @staticmethod
+    def slice_by_cond(df, cond):
+        """ Return slices splitting the dataframe by a certain condition. """
+        idxs = df.loc[cond].index
+        slices = []
+        last_idx = 0
+        for idx in idxs:
+            slices.append(slice(last_idx, idx + 1))
+            last_idx = idx + 1
+        return slices
+    
+    @staticmethod
+    def split_by_rows(df, slices):
+        dfs = []
+        for s in slices:
+            dfs.append(df.iloc[s])
+        return dfs
+
+    @staticmethod
+    def split_by_cols(df, slices):
+        dfs = []
+        for s in slices:
+            dfs.append(df.iloc[:, s].dropna(how='all', axis=1).dropna(how='all', axis=0))
+        return dfs
+
+    @staticmethod
+    def first_row_to_columns(df):
+        """ Replaces the columns of a dataframe with the values in the first row. Drops that row. """
+        df.columns = df.iloc[0].values
+        return df[1:]
+
+    @staticmethod
+    def repeat(df, cond):
+        """ Repeat rows of a dataframe where a condition is met. """
+        return pd.concat([df, df.loc[cond]]).sort_index().reset_index(drop=True)
 
 
 class OSBTable(Table):
@@ -299,7 +335,101 @@ class Illinois(OSBTable):
         df = df.sort_values(by=['Date', 'Provider', 'Sport Level', 'Sub-Category'], ascending=True)
         OSBTable.save(Illinois, df)
 
+class Indiana(Table):
+    state = 'Indiana'
+    xlsx_date = date(2019, 7, 1)
 
+    def __init__(self, dt):
+        self.date = dt
+        timestamp = self.date.strftime("%Y-%m")
+        self.url = f'https://www.in.gov/igc/files/{timestamp}-Revenue.xlsx'
+        self.gaming_df = self.original_gaming()
+        self.sports_df = self.original_sports_betting()
+
+    def original_gaming(self):
+        """ HTML/PDF before July 2019. """
+        # First sheet is casinos.
+        df = pd.read_excel(self.url, sheet_name=0, skiprows=3)
+        return df.dropna(how='all', subset=df.columns[1:], ignore_index=True).dropna(how='all', axis=1)
+
+    def clean_gaming(self):
+        df = self.gaming_df
+        # Data is split into three groups, detectable by TOTAL row.
+        slices = self.slice_by_cond(df, df['TOTAL TAX'] == 'TOTAL')
+        # Split and get new cols.
+        a, b, c = self.split_by_rows(df, slices)
+        b = self.first_row_to_columns(b)
+        c = self.first_row_to_columns(c)
+        # Remove extra cols and backfill data where needed.
+        b = (b.iloc[:,2:].
+             fillna(method='ffill').
+             reset_index(drop=True))
+        c = (self.repeat(c, c['WAGERING TAX'] == 'Hard Rock Casino Northern Indiana').
+             iloc[:,1:])
+        out_df = pd.concat([a, b, c], axis=1)
+        out_df.insert(0, 'State', self.state)
+        out_df.insert(1, 'Category', 'iGaming')
+        out_df.insert(2, 'Date', self.date)
+        out_df.rename(columns={'TOTAL TAX': 'Provider'}, inplace=True)
+        numeric_cols = ['Supplemental Tax', 'Sports Wagering Tax', 'Wagering Tax', 'Total Tax',
+                'Win', 'Free Play', 'Other *', 'Taxable AGR', 'No. of Table Games',
+                'Table Win', 'No. of EGD/Slots', 'EGD/Slot Win', 'AGR']
+        Table.to_numeric(out_df, numeric_cols)
+        return out_df
+
+    def original_sports_betting(self):
+        """ Sports betting was not recorded before September 2019, in Indiana. """
+        if self.date < datetime(2019, 9, 1):
+            return None
+        else:
+            # Last sheet is sports betting.
+            return pd.read_excel(self.url, sheet_name=-1, skiprows=3)
+
+    def clean_sports_betting(self):
+        if self.sports_df is None:
+            return None
+        split_dfs = self.split_by_cols(self.sports_df, [slice(0, 4), slice(5, 9), slice(10, 14)])
+        parsed_dfs = [self.parse_sports_wagers(x) for x in split_dfs]
+        out_df = pd.concat(parsed_dfs).reset_index(drop=True)
+        return out_df
+    
+    def parse_sports_wagers(self, df):
+        out_df = []
+        provider = None
+        should_add_row = False
+        total_handle = 0
+        for sub, handle, gross in df.itertuples(index=False):
+            if handle == "Handle":
+                provider = sub
+                should_add_row = True
+                continue
+            if should_add_row:
+                # Adjustments handle is empty string.
+                if sub == "Adjustments":
+                    pass
+                elif sub != "Taxable AGR":
+                    total_handle += handle
+                else:
+                    sub = "Total"
+                    should_add_row = False
+                    handle = total_handle
+                    total_handle = 0
+                out_df.append({'State': self.state, 'Category': 'Online Sports Betting (OSB)', 'Date': self.date, 
+                               'Provider': provider, 'Sub-Provider': sub, 'Handle': handle, 'AGR': gross})
+        return pd.DataFrame(out_df)
+      
+    @staticmethod
+    def save_games(df):
+        df = IGamingTable.combine_old(Indiana, df)
+        df = df.sort_values(by=['Date', 'Provider'], ascending=True)
+        IGamingTable.save(Indiana, df)
+
+    @staticmethod
+    def save_sports(df):
+        df = OSBTable.combine_old(Indiana, df)
+        df.index.name = 'Index'
+        df = df.sort_values(by=['Date', 'Index'], ascending=True)
+        OSBTable.save(Indiana, df)
 
 ### Scraping functions ###
 def scrape_arizona():
@@ -365,9 +495,24 @@ def scrape_illinois():
     Illinois.save(df)
     print("Finished Illinois".center(50, '-'))
 
+def scrape_indiana():
+    print("Starting Indiana".center(50, '-'))
+    games_data, sports_data = [], []
+    for dt in get_dates(date(2019, 9, 1)):
+        try:
+            print(f"Scraping {dt}")
+            x = Indiana(dt)
+            games_data.append(x.clean_gaming())
+            sports_data.append(x.clean_sports_betting())
+        except:
+            print(f"**Unable to scrape {dt}")
+    Indiana.save_games(pd.concat(games_data))
+    Indiana.save_sports(pd.concat(sports_data))
+    print("Finished Indiana".center(50, '-'))
 
 
 if __name__ == '__main__':
     #scrape_arizona()
-    scrape_connecticut()
+    #scrape_connecticut()
     #scrape_illinois()
+    scrape_indiana()
