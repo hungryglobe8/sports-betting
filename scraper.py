825eacca4d3ffe7d987283b820273d2a153256b0
Casey Rand <casrand@gmail.com>
2023-06-02 11:46:23 -0500

    Scraper working for AZ

--- scraper.py
+++ scraper.py
@@ -6,6 +6,9 @@ from dateutil.rrule import rrule, MONTHLY
 from dateutil.relativedelta import relativedelta
 from time import sleep
 from pathlib import Path
+import re
+from PyPDF2 import PdfReader
+import requests
 
 
 def get_dates(start, end=None):
@@ -14,8 +17,11 @@ def get_dates(start, end=None):
         end = date.today().replace(day=1)
     return list(rrule(MONTHLY, dtstart=start, until=end))
 
+def extract_date(text, regex, datefmt):
+    """ Extract a date from a text through regex and datefmt. """
+    return datetime.strptime(re.search(regex, text)[0], datefmt)
+
 class Table:
-    
     @staticmethod
     def categorize(col, categories):
         """ Returns a category column, which is helpful for sorting. """
@@ -48,6 +54,101 @@ class OSBTable(Table):
 
 class IGamingTable(Table):
     pass
+
+
+class Arizona(OSBTable):
+    state = 'Arizona'
+    url = "https://gaming.az.gov/resources/reports#event-wagering-report-archive"
+
+    def __init__(self, url):
+        self.url = url
+        self.date = self.find_timestamp(self.url)
+
+    def clean(self):
+        path = Path('temp.pdf')
+        path.write_bytes(requests.get(self.url).content)
+        pdf = PdfReader(str(path))
+        
+        data = []
+        # Skip first line.
+        for line in pdf.pages[0].extract_text().split('\n')[1:]:
+            provider = self.get_provider(line)
+            if provider == '':
+                break
+            values = self.get_numerical(line)
+            data.append({
+                'State': self.state,
+                'Category': self.category,
+                'Sub-Category': 'Retail',
+                'Date': self.date,
+                'Provider': provider,
+                'Gross Wagering Receipts': values[0],
+                'Amount Won': values[2],
+                'Adjusted Gross Wagering Receipts': values[4],
+                'Promotional Credits': values[6]
+            })
+            data.append({
+                'State': self.state,
+                'Category': self.category,
+                'Sub-Category': 'Online',
+                'Date': self.date,
+                'Provider': provider,
+                'Gross Wagering Receipts': values[1],
+                'Amount Won': values[3],
+                'Adjusted Gross Wagering Receipts': values[5],
+                'Promotional Credits': values[7]
+            })
+        path.unlink()
+        return pd.DataFrame(data).replace(0, pd.NA).dropna(thresh=6)
+    
+    @staticmethod
+    def find_timestamp(url):
+        url = url.replace('%20', ' ')
+        match = re.search(r'\w+ \d{4}', url)[0]
+        match = f'{match[:3]} {match[-4:]}'
+        return datetime.strptime(match, '%b %Y')
+    
+    @staticmethod
+    def get_provider(line):
+        values = line.split()
+        # Find provider by everything before first '-', '$', or numeric value.
+        provider = []
+        for val in values:
+            if val == '-' or val == '$':
+                break
+            else:
+                val = val.replace(',', '')
+                try:
+                    float(val)
+                    break
+                except ValueError:
+                    provider.append(val)
+        return ' '.join(provider)
+
+    @staticmethod
+    def get_numerical(line):
+        values = line.split()
+        # Find values by '-', or numeric values.
+        numerical = []
+        for val in values:
+            # Remove trailing '$'.
+            val = val.strip('$')
+            if '-' in val:
+                numerical.append(0)
+            else:
+                val = val.replace(',', '')
+                try:
+                    numerical.append(float(val))
+                except ValueError:
+                    continue
+        return numerical
+
+    @staticmethod
+    def save(df):
+        df = OSBTable.combine_old(Arizona, df)
+        df['Sub-Category'] = Table.categorize(df['Sub-Category'], ['Retail', 'Online', 'Total'])
+        df = df.sort_values(by=['Date', 'Provider', 'Sub-Category'], ascending=True)
+        OSBTable.save(Arizona, df)
 
 
 class Illinois(OSBTable):
@@ -109,6 +210,45 @@ class Illinois(OSBTable):
         OSBTable.save(Illinois, df)
 
 
+
+### Scraping functions ###
+def scrape_arizona():
+    print("Starting Arizona".center(50, '-'))
+    dataframes = []
+    # Arizona urls are hard-coded. Cannot be parsed automatically.
+    links = [
+        "https://gaming.az.gov/sites/default/files/EW%20Website%20Report%20-%20Sept%202021.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Website%20Report%20-%20Oct%202021.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Report%20for%20Website%20-%20Nov%202021.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website-Dec%202021.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20-%20Jan%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Rpt%20for%20Website%20-%20Feb%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20-%20Mar%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20April%202022%20Revenue%20Report.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Website%20Revenue%20Report-May%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20June%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20July%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Report%20for%20Website%20-%20Aug%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20-Sep%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20-%20Oct%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20-%20Nov%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20-%20Dec%202022.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20-%20Jan%202023.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Website%20Revenue%20Report-Feb%202023.pdf",
+        # Attempt March in two formats.
+        "https://gaming.az.gov/sites/default/files/EW%20Revenue%20Report%20for%20Website%20-%20Mar%202023.pdf",
+        "https://gaming.az.gov/sites/default/files/EW%20Website%20Revenue%20Report-Mar%202023.pdf"]
+    for link in links:
+        print(f"Scraping {link}")
+        try:
+            dataframes.append(Arizona(link).clean())
+        except:
+            Path('temp.pdf').unlink()
+            print(f"**Unable to scrape {link}")
+    df = pd.concat(dataframes)
+    Arizona.save(df)
+    print("Finished Arizona".center(50, '-'))
+
 def scrape_illinois():
     print("Starting Illinois".center(50, '-'))
     driver = Illinois.selenium()
@@ -126,4 +266,6 @@ def scrape_illinois():
 
 
 if __name__ == '__main__':
-    scrape_illinois()
+    #scrape_arizona()
+    
+    #scrape_illinois()
